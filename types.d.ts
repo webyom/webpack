743275be05f78d0ef72ffafb8d895b9ac255c89f
Tobias Koppers <tobias.koppers@googlemail.com>
2020-08-01 06:21:29 +0200

    display assets in stats in a list instead of a table group related assets below the parent asset

--- types.d.ts
+++ types.d.ts
@@ -8118,6 +8118,11 @@ declare interface StatsOptions {
 	reasons?: boolean;
 
 	/**
+	 * Add information about assets that are related to other assets (like SourceMaps for assets).
+	 */
+	relatedAssets?: boolean;
+
+	/**
 	 * Add information about runtime modules.
 	 */
 	runtime?: boolean;
